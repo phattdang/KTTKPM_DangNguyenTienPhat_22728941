38c3d356062afc3f25a75bc306135ae9ba5b788e
phatdang <phatdang19052004@gmail.com>
2026-03-18 19:11:26 +0700

    Khởi tạo project tuần 03

---
+++ KTTKPM_DangNguyenTienPhat_22728941_Tuan03/src/main/java/fit/iuh/se/Main.java
@@ -0,0 +1,12 @@
+package fit.iuh.se;
+
+/**
+ * @author : user664dntp
+ * @mailto : phatdang19052004@gmail.com
+ * @created : 18/03/2026, Wednesday
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
